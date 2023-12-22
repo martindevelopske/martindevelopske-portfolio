72755589aebbef928fded4e5a0843a3820728b01
Martin Ndung'u <martindevelopske@gmail.com>
2023-12-23 01:13:47 +0300

    m

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -9,7 +9,7 @@ function ProjectsPage() {
       className="h-auto text-black bg-greyish dark:bg-gradient-to-b from-1e to-black w-full flex flex-col items-center justify-between dark:text-white"
       data-name="projects"
     >
-      <div className="flex justify-between items-center h-64 w-full px-3 fixed top-0 left-0 z-50 dark:bg-blackbg">
+      <div className="flex justify-between items-center h-64 w-full px-3 fixed top-0 left-0 z-50 bg-white dark:bg-blackbg">
         <Link
           to="/"
           className=" text-lg text-green-500 flex items-center justify-center gap-2"
