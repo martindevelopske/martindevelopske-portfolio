ffff5cd8e9faae73b68e3d55945d3d32787e67e5
Martin Ndung'u <martindevelopske@gmail.com>
2024-01-09 04:30:59 +0300

    resume updated

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,5 @@ COPY . .
 FROM node:21-alpine
 WORKDIR /app
 COPY --from=build /app .
+EXPOSE 5173
 CMD [ "npm", "run", "dev" ]
--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,5 +1,3 @@
-// import Header from "../components/Header";
-
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import { IconsData } from "../data/Iconsdata";
@@ -12,9 +10,10 @@ const Resume = () => {
       </div>
       <div className="w-full md:w-5/6 lg:w-3/4 mt-14">
         <div className="max-w-2xl mx-auto p-6 bg-white dark:bg-blackbg light:text-black dark:text-white rounded-md">
+          {/* Personal Information */}
           <div className="max-w-2xl mx-auto mt-10 p-4 rounded-md text-center">
             <h1 className="text-base font-bold mb-4 text-green-600">
-              Martin Ndung'u - Full Stack Developer & DevOps Engineer
+              Martin Ndung'u - Full Stack Software Engineer
             </h1>
             <div className="max-w-2xl mx-auto p-2 w-full md:w-3/4 rounded-md text-center text-sm md:text-base">
               <p className="font-bold mb-4">
@@ -27,85 +26,85 @@ const Resume = () => {
                   martindevelopske@gmail.com
                 </Link>
               </p>
-              {/* ... (rest of your content) */}
             </div>
           </div>
-
+          {/* Summary Section */}
           <p className="text-md font-mono">
-            I have extensive experience developing and executing top-notch web
-            apps as a seasoned full-stack developer. With a strong background in
-            server-side architecture and seamless user experiences, I am
-            proficient in both front-end and back-end technologies. I am
-            committed to providing creative solutions and keeping up with the
-            most recent business trends in order to effectively contribute to
-            any development project. I have a track record of success in this
-            regard.
+            Experienced Fullstack Developer with a strong background in
+            server-side architecture and seamless user experiences. Proficient
+            in both front-end and back-end technologies. Committed to providing
+            creative solutions and staying updated on recent business trends to
+            contribute effectively to development projects.
           </p>
 
-          {/* Add more sections as needed */}
-
+          {/* Experience Section */}
           <h2 className="text-base font-bold mt-4 mb-2 text-green-600">
             Experience
           </h2>
+          <ul className="text-md">
+            <li>
+              <strong>Songa: Full-stack Engineer (LinkedIn)</strong>
+              <br />
+              (Jan 2023 - Sep 2023)
+              <ul>
+                <li className="list-disc ml-6">
+                  Established server-side logic and database structures for
+                  reliable web app
+                </li>
+                <li className="list-disc ml-6">
+                  Created effective server-side APIs facilitating smooth
+                  communication between front-end and back-end
+                </li>
+                <li className="list-disc ml-6">
+                  Set up and maintained web servers for application
+                  dependability and peak performance
+                </li>
+                <li className="list-disc ml-6">
+                  Expert in debugging and diagnosing application failures with a
+                  methodological approach
+                </li>
+                <li className="list-disc ml-6">
+                  Collaborated with cross-functional teams including designers
+                  and front-end engineers
+                </li>
+              </ul>
+            </li>
+            <li>
+              <strong>Freelance Developer</strong>
+              <br />
+              (2022 - present)
+              <ul>
+                <li className="list-disc ml-6">
+                  Collaborated with diverse clients to deliver custom websites
+                  and web applications
+                </li>
+                <li className="list-disc ml-6">
+                  Translated business requirements into functional code and
+                  ensured seamless integration with existing systems
+                </li>
+                <li className="list-disc ml-6">
+                  Leveraged expertise in front-end and back-end technologies to
+                  create user-friendly applications
+                </li>
+                <li className="list-disc ml-6">
+                  Consistently met project deadlines
+                </li>
+              </ul>
+            </li>
+          </ul>
 
-          <div className="text-md">
-            <div className="flex justify-between">
-              <strong className="text-red-400">
-                Songa: Back-end Engineer (
-                <Link
-                  to="https://www.linkedin.com/company/songaapp/?lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_all%3B1JKUV3VZRuKNgXpz6fLz9g%3D%3D"
-                  target="__blank"
-                  className=" text-blue-600"
-                >
-                  LinkedIn
-                </Link>
-                )
-              </strong>
-              Jan 2023 - Sep 2023
-            </div>
-            <br />
-            As a full-stack developer, I was able to effectively establish
-            server-side logic and database structures, which helped build
-            reliable apps. Furthermore, I was an expert in creating effective
-            server-side APIs that enabled smooth communication between the
-            front-end and back-end parts. I set up and maintained web servers to
-            guarantee application dependability and peak performance. When
-            problems occurred, I was excellent at debugging and diagnosing
-            application failures, using a methodical manner to quickly fix
-            problems. My job was centered around collaboration; to create new
-            and well-coordinated solutions, I worked closely with designers,
-            product managers, and other engineers in cross-functional teams.
-          </div>
-          <br />
-          <div className="text-md">
-            <div className="flex justify-between items-center">
-              <strong className="text-red-400">Freelance Developer</strong>
-              2022 - present
-            </div>
-            <br />
-            As a freelance developer, I successfully collaborated with diverse
-            clients to deliver custom websites and web application solutions
-            tailored to their specific needs. I leveraged my expertise in
-            front-end and back-end technologies to create user-friendly and
-            efficient applications that met client expectations. My work
-            involved translating business requirements into functional code,
-            ensuring seamless integration with existing systems, and
-            consistently meeting project deadlines.
-          </div>
-
-          {/* Add more experience entries as needed */}
-
+          {/* Technical Skills Section */}
           <h2 className="text-base font-bold mt-4 mb-2 text-green-600">
             Technical Skills
           </h2>
 
           <ul className="list-disc ml-6 text-md">
             <li>
-              <strong>Front-end:</strong> React, Nextjs, Typescript, Javascript,
+              <strong>Front-End:</strong> React, Nextjs, Typescript, Javascript,
               CSS, Tailwind, Bootstrap, MUI, HTML.
             </li>
             <li>
-              <strong>Back-end:</strong> NodeJs, Javascript, Typescript,
+              <strong>Back-End:</strong> NodeJs, Javascript, Typescript,
               Express, Prisma ORM.
             </li>
             <li>
@@ -116,61 +115,48 @@ const Resume = () => {
               Terraform, AWS.
             </li>
           </ul>
-
-          {/* Add more sections as needed */}
-
+          {/* Projects Section */}
           <h2 className="text-base font-bold mt-4 mb-2 text-green-600">
             Projects
           </h2>
 
-          <p className="text-md">
-            <strong className="text-red-400">
-              Songa App{" "}
-              <Link
-                to="https://www.linkedin.com/company/songaapp/?lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_all%3B1JKUV3VZRuKNgXpz6fLz9g%3D%3D"
-                target="__blank"
-                className=" text-blue-600"
-              >
-                (View)
-              </Link>
-            </strong>
-            <br />
-            Songa is a ride service application which bridges the gap between
-            riders in Kenya and their clients. Built with Node JS, express,
-            postgresql, prisma and Next js. Allows users to request for rides,
-            get rides, pay for rides and complete rides.
-          </p>
-          <br />
-          <p className="text-md">
-            <strong className="text-red-400">
-              Quick-sale{" "}
-              <Link to="" className=" text-blue-600">
-                (View)
-              </Link>
-            </strong>
-            <br />
-            An E-commerce web application built with React JS, node JS,express,
-            and mongoDB. This allows users to authenticate, add products, check
-            inventory and also customers to make purchases.
-          </p>
-          <br />
-          <div className="text-md">
-            You can take a look at more projects on my portfolio.{" "}
-            <Link to="/projects" className="text-blue-400">
-              Projects
-            </Link>
-          </div>
-          {/* Add more project entries as needed */}
-
+          <ul>
+            {/* Songa App Project */}
+            <li>
+              <strong>Songa App</strong> (View)
+              <ul>
+                <li className="list-disc ml-6">
+                  Ride service application built with Node JS, Express,
+                  Postgresql, Prisma, and Next JS.
+                </li>
+                <li className="list-disc ml-6">
+                  Allows users to request, get, pay for, and complete rides.
+                </li>
+              </ul>
+            </li>
+            {/* E-commerce Site Project */}
+            <li>
+              <strong>E-commerce Site</strong> (View)
+              <ul>
+                <li className="list-disc ml-6">
+                  Web application built with React JS, Node JS, Express, and
+                  MongoDB.
+                </li>
+                <li className="list-disc ml-6">
+                  Features authentication, product management, inventory checks,
+                  and customer purchases.
+                </li>
+              </ul>
+            </li>
+          </ul>
+          {/* Interests Section */}
           <h2 className="text-base font-bold mt-4 mb-2 text-green-600">
             Interests
           </h2>
-
           <p className="text-md">
             I am a huge football and basketball fan, also playing both during my
             free time. I also love coding things out for fun.
           </p>
-
           <h2 className="text-base font-bold mt-4 mb-2 text-green-600">
             Important Links
           </h2>
@@ -209,5 +195,4 @@ const Resume = () => {
     </div>
   );
 };
-
 export default Resume;
